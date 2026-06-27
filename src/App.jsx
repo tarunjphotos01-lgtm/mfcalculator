093088a57470eec3f7eec03ff5100a2740523740
Taruntj <tarun.jeengar@newvision-software.com>
2026-06-28 01:15:49 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Pages/LoginPage";
-import Home from "./Pages/Home";
+import MFProfitLossCalculator from "./Pages/MFProfitLossCalculator";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<MFProfitLossCalculator/>} />
       </Routes>
     </Router>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./Redux/Store";
-import ThemeContextProvider from "./Pages/ThemeContext"; // <-- import your Theme provider
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <Provider store={store}>
         <App />
-      </Provider>
-    </ThemeContextProvider>
   </React.StrictMode>
 );
 
